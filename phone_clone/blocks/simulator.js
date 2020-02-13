85a490b0aaa0392c9dc27a69cd9a7be780b9c914
David Watson <david@neonquill.com>
2020-02-13 18:06:42 -0500

    Create motor functions

--- phone_clone/blocks/simulator.js
+++ phone_clone/blocks/simulator.js
@@ -9,8 +9,33 @@ var linearOpMode = {
     } else {
       return false;
     }
-  }
+  },
+  sleep: async function(ms) {
+    // XXX
+  },
 };
+
+function DcMotor(name) {
+  this.name = name;
+
+  this.setDirection = function(dir) {
+    this.direction = dir;
+  };
+
+  this.setPower = function(power) {
+    this.power = power;
+  };
+
+  this.setDualPower = function(self_power, other, other_power) {
+    this.setPower(self_power);
+    other.setPower(other_power);
+  };
+};
+
+var FLmotorAsDcMotor = new DcMotor("FLmotor");
+var FRmotorAsDcMotor = new DcMotor("FRmotor");
+var BLmotorAsDcMotor = new DcMotor("BLmotor");
+var BRmotorAsDcMotor = new DcMotor("BRmotor");
 
 var telemetry = {
   update: function() {}
