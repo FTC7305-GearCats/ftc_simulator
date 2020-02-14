799a25ac394d2344d9bba9788eb81996e258dbbf
David Watson <david@neonquill.com>
2020-02-13 20:13:55 -0500

    Switch to using JS-Interpreter

--- phone_clone/blocks/simulator.js
+++ phone_clone/blocks/simulator.js
@@ -1,17 +1,18 @@
+// simulator time in ms.
+var simulatorTime = 0;
+
+function getSimulatorTime() {
+  return simulatorTime;
+}
+
 var linearOpMode = {
-  count: 20,
   waitForStart: function() {},
   opModeIsActive: function() {
-    linearOpMode.count -= 1;
-    console.log(linearOpMode.count);
-    if (linearOpMode.count >= 0) {
-      return true;
-    } else {
-      return false;
-    }
+    return true;
   },
-  sleep: async function(ms) {
+  sleep: function(ms) {
     // XXX
+    console.log("sleep", ms);
   },
 };
 
@@ -32,23 +33,69 @@ function DcMotor(name) {
   };
 };
 
-var FLmotorAsDcMotor = new DcMotor("FLmotor");
-var FRmotorAsDcMotor = new DcMotor("FRmotor");
-var BLmotorAsDcMotor = new DcMotor("BLmotor");
-var BRmotorAsDcMotor = new DcMotor("BRmotor");
-
 var telemetry = {
   update: function() {}
+};
+
+function Robot() {
+  this.setPower = function(motor, power) {
+    console.log(motor, power);
+  };
+}
+
+var realRobot = new Robot();
+
+var createDcMotor = function(interpreter, scope, name) {
+  var motor = interpreter.nativeToPseudo({});
+  interpreter.setProperty(scope, name, motor);
+
+  interpreter.setProperty(motor, 'name', name);
+
+  var setDirection = function(dir) {
+    console.log("setDirection");
+  };
+  interpreter.setProperty(motor, 'setDirection',
+      interpreter.createNativeFunction(setDirection));
+
+  var setPower = function(power) {
+    realRobot.setPower(name, power);
+  };
+  interpreter.setProperty(motor, 'setPower',
+      interpreter.createNativeFunction(setPower));
+
+  var setDualPower = function(self_power, other, other_power) {
+    realRobot.setPower(name, self_power);
+    realRobot.setPower(other.properties.name, other_power);
+  };
+  interpreter.setProperty(motor, 'setDualPower',
+      interpreter.createNativeFunction(setDualPower));
+};
+
+var initFunc = function(interpreter, scope) {
+  var lom = interpreter.nativeToPseudo(linearOpMode);
+  interpreter.setProperty(scope, 'linearOpMode', lom);
+
+  var tel = interpreter.nativeToPseudo(telemetry);
+  interpreter.setProperty(scope, 'telemetry', tel);
+
+  createDcMotor(interpreter, scope, "FLmotorAsDcMotor");
+  createDcMotor(interpreter, scope, "FRmotorAsDcMotor");
+  createDcMotor(interpreter, scope, "BLmotorAsDcMotor");
+  createDcMotor(interpreter, scope, "BRmotorAsDcMotor");
 };
 
 function runSimulator() {
   Blockly.JavaScript.addReservedWords('code');
   var code = Blockly.JavaScript.workspaceToCode(workspace);
-  code = Function(code + "runOpMode();");
-  try {
-    code();
-  } catch (e) {
-    console.log(e);
+  var myInterpreter = new Interpreter(code, initFunc);
+  myInterpreter.appendCode('runOpMode();');
+
+
+  function nextStep() {
+    if (myInterpreter.step()) {
+      window.setTimeout(nextStep, 10);
+    }
   }
+  nextStep();
 }
 
