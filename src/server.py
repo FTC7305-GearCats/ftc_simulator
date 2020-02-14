9ceb5c6e5078a07722c1520bb9ec692abc7defd0
David Watson <david@neonquill.com>
2020-02-14 17:03:13 -0500

    Get listing to work

--- src/server.py
+++ src/server.py
@@ -2,8 +2,12 @@
 
 import bottle
 import sqlite3
+import json
 
-conn = sqlite3.connect("../data/blocks.db")
+sqlite3.register_converter("BOOLEAN", lambda v: bool(int(v)))
+conn = sqlite3.connect("../data/blocks.db",
+                       detect_types=sqlite3.PARSE_DECLTYPES)
+conn.row_factory = sqlite3.Row
 
 @bottle.route("/fetch_blk")
 def fetch_blk():
@@ -12,9 +16,10 @@ def fetch_blk():
 @bottle.route("/list")
 def list():
     c = conn.cursor()
-    c.execute("""SELECT (dateModifiedMillis, enabled, escapedName, name)
+    c.execute("""SELECT dateModifiedMillis, enabled, escapedName, name
                  FROM blocks""")
-    print(c.fetchall())
+    data = c.fetchall()
+    return json.dumps([dict(r) for r in data])
 
 @bottle.route("/<path:path>")
 def static(path):
