0f119c2f15abf9409db442d867197e61fbd11de1
David Watson <david@neonquill.com>
2021-10-21 13:42:59 -0400

    Fix places with the `if  (` pattern (two spaces)

--- munge.sh
+++ munge.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 # Force the http logic to always fail.
-rg -l0 "if \(window.location.protocol === 'http" static_new | \
+rg -l0 "if\s+\(window.location.protocol === 'http" static_new | \
   xargs -0 sed -i "" \
-  's/\(if (\)\(window.location.protocol === '\''http.*\))/\1false \/* \2 *\/)/g'
+  's/\(if[[:space:]]*(\)\(window.location.protocol === '\''http.*\))/\1false \/* \2 *\/)/g'
 
 # Force the localhost logic to always succeed.
-rg -l0 "if \(window.location.protocol === 'file" static_new | \
+rg -l0 "if\s+\(window.location.protocol === 'file" static_new | \
   xargs -0 sed -i "" \
-  's/\(if (\)\(window.location.protocol === '\''file\)/\1true || \2/g'
+  's/\(if[[:space:]]*(\)\(window.location.protocol === '\''file\)/\1true || \2/g'
